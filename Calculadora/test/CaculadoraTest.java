8ed9e6620747360d035b9f139d2b00037fcc4eff
camilo4785 <59936745+camilo4785@users.noreply.github.com>
2020-01-15 15:35:55 -0500

    Add files via upload

---
+++ Calculadora/test/CaculadoraTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author camilo.campos
+ */
+public class CaculadoraTest {
+    
+    public CaculadoraTest() {
+    }
+    @Test
+    public void testSuma(){
+        int resultado = Calculadora.Suma(2, 3);
+        int esperado = 5;
+       assertEquals(esperado, resultado);
+    }
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
